2ca25bc63b5ac56eaab17ec83e293ea7bf64d02d
Mahmoud Ahmed <ebrahimahmed2246@gmail.com>
2024-07-29 14:43:35 +0300

    add bloc

---
+++ lib/src/core/bloc/base_bloc/bloc_screen_builder.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'base_bloc.dart';
+import 'base_state.dart';
+
+class BaseBlocBuilder<T> extends StatelessWidget {
+  final BaseBloc<T> bloc;
+  final Widget Function(T data) onSuccessWidget;
+  final Widget? onFailedWidget;
+  final Widget? onLoadingWidget;
+
+  const BaseBlocBuilder({
+    Key? key,
+    required this.bloc,
+    required this.onSuccessWidget,
+    this.onFailedWidget,
+    this.onLoadingWidget,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BaseBloc<T>, BaseState<T>>(
+      bloc: bloc,
+      buildWhen: (previous, current) => previous != current,
+      builder: (_, state) {
+        return state.maybeWhen(
+          orElse: () => SizedBox.shrink(),
+          success: (data) {
+            return onSuccessWidget(data!);
+          },
+          loading: () => onLoadingWidget ?? const CupertinoActivityIndicator(),
+          failure: (_, __) => onFailedWidget ?? Text(state.toString()),
+        );
+      },
+    );
+  }
+}
